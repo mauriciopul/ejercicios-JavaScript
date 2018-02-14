2bf605d40740f24ec7dbed482f6e1238e65d7712
mao <mauriciopul@hotmail.com>
2018-02-14 17:22:10 -0500

    Crear matriz 12 automoviles y filtro por puertas

--- index.js
+++ index.js
@@ -40,3 +40,29 @@ auto.numeroPuertas = "4";
 
 
 console.log(auto.mostrarDatos())
+
+
+var automoviles = [
+	{color:"amarillo", placa:"asd435", modelo:"2007",numeroPuertas:"3"},
+	{color:"plateado", placa:"mnh235", modelo:"2090",numeroPuertas:"4"},
+	{color:"gris", placa:"oiu843", modelo:"1900",numeroPuertas:"5"},
+	{color:"Rojo Oscuro", placa:"sdr475", modelo:"1906",numeroPuertas:"3"},
+	{color:"magenta", placa:"xdr323", modelo:"1940",numeroPuertas:"4"},
+	{color:"fofo", placa:"cgt980", modelo:"1997",numeroPuertas:"2"},
+	{color:"negro", placa:"sds332", modelo:"2009",numeroPuertas:"6"},
+	{color:"fucsia", placa:"llo999", modelo:"2019",numeroPuertas:"4"},
+	{color:"violeta", placa:"scr454", modelo:"2010",numeroPuertas:"3"},
+	{color:"naranja", placa:"hhy309", modelo:"1990",numeroPuertas:"6"},
+	{color:"rojo", placa:"rrp332", modelo:"1999",numeroPuertas:"4"},
+	{color:"verde", placa:"ffg555", modelo:"1989",numeroPuertas:"8"}
+]
+
+var cuatroPuertas = automoviles.filter(function(item){
+    return item.numeroPuertas == 4;
+})
+console.log(cuatroPuertas);
+
+var tresPuertas = automoviles.filter(function(item){
+    return item.numeroPuertas == 3;
+})
+console.log(tresPuertas);
